89d87b6a489e6198ccd7b5b9fcb65220b8963ff8
MomentumⓂ️Ⓜ️T <1336423652@qq.com>
2025-10-30 22:27:37 +0800

    Update aura.ts

--- fees/aura.ts
+++ fees/aura.ts
@@ -2,7 +2,7 @@ import { Adapter, FetchOptions, FetchResultFees } from "../adapters/types";
 import { CHAIN } from "../helpers/chains";
 import { queryIndexer } from "../helpers/indexer";
 
-const BAL_TOKEN = '0xba100000625a3754423978a60c9317c58a424e3D';
+const MMT_TEST_TOKEN = '0x6f546b37018a67fa80da59f6391dc7a13e4d4444';
 
 const fetch = async (options: FetchOptions) => {
   const dailyFees = options.createBalances();
